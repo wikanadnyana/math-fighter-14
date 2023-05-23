9de5a740cf022dd8b2332272a832b607780d8c29
wikanadnyana <wikanadnyana44@gmail.com>
2023-05-23 17:18:36 +0800

    [ADD] new method

--- src/scenes/MathFighterScene.js
+++ src/scenes/MathFighterScene.js
@@ -9,6 +9,21 @@ export default class MathFighterScene extends Phaser.Scene {
     this.player = undefined;
     this.enemy = undefined;
     this.slash = undefined;
+    this.startGame = false;
+    this.questionText = undefined;
+    this.resultText = undefined;
+    this.button1 = undefined;
+    this.button2 = undefined;
+    this.button3 = undefined;
+    this.button4 = undefined;
+    this.button5 = undefined;
+    this.button6 = undefined;
+    this.button7 = undefined;
+    this.button8 = undefined;
+    this.button9 = undefined;
+    this.button0 = undefined;
+    this.buttonDel = undefined;
+    this.buttonOk = undefined;
   }
   preload() {
     this.load.image("background", "images/bg_layer1.png");
@@ -30,6 +45,7 @@ export default class MathFighterScene extends Phaser.Scene {
       frameWidth: 42,
       frameHeight: 88,
     });
+    this.load.image("start-btn", "images/start_button.png");
   }
   create() {
     this.add.image(240, 320, "background");
@@ -61,6 +77,19 @@ export default class MathFighterScene extends Phaser.Scene {
       .setCollideWorldBounds(true);
 
     this.createAnimation();
+    let start_button = this.add
+      .image(this.gameHalfWidth, this.gameHalfHeight + 181, "start-btn")
+      .setInteractive();
+
+    start_button.on(
+      "pointerdown",
+      () => {
+        this.gameStart();
+        start_button.destroy();
+      },
+      this
+    );
+    this.createButtons();
   }
   update() {}
 
@@ -110,5 +139,104 @@ export default class MathFighterScene extends Phaser.Scene {
       frames: this.anims.generateFrameNumbers("enemy", { start: 0, end: 4 }),
       frameRate: 10,
     });
+  }
+  gameStart() {
+    this.startGame = true;
+    this.player.anims.play("player-standby", true);
+    this.enemy.anims.play("enemy-standby", true);
+    this.resultText = this.add.text(this.gameHalfWidth, 200, "0", {
+      fontSize: "32px",
+      fill: "#000",
+    });
+    this.questionText = this.add.text(this.gameHalfWidth, 100, "0", {
+      fontSize: "32px",
+      fill: "#000",
+    });
+  }
+
+  createButtons() {
+    const startPosY = this.scale.height - 246;
+    const widthDiff = 131;
+    const heightDiff = 71.25;
+    // center buttons
+    this.button2 = this.add
+      .image(this.gameHalfWidth, startPosY, "numbers", 1)
+      .setInteractive()
+      .setData("value", 2);
+    this.button5 = this.add
+      .image(this.gameHalfWidth, this.button2.y + heightDiff, "numbers", 4)
+      .setInteractive()
+      .setData("value", 5);
+    this.button8 = this.add
+      .image(this.gameHalfWidth, this.button5.y + heightDiff, "numbers", 7)
+      .setInteractive()
+      .setData("value", 8);
+    this.button0 = this.add
+      .image(this.gameHalfWidth, this.button8.y + heightDiff, "numbers", 10)
+      .setInteractive()
+      .setData("value", 0);
+    //left side
+    this.button1 = this.add
+      .image(this.button2.x - widthDiff, startPosY, "numbers", 0)
+      .setInteractive()
+      .setData("value", 1);
+    this.button4 = this.add
+      .image(
+        this.button5.x - widthDiff,
+        this.button1.y + heightDiff,
+        "numbers",
+        3
+      )
+      .setInteractive()
+      .setData("value", 4);
+    this.button7 = this.add
+      .image(
+        this.button8.x - widthDiff,
+        this.button4.y + heightDiff,
+        "numbers",
+        6
+      )
+      .setInteractive()
+      .setData("value", 7);
+    this.buttonDel = this.add
+      .image(
+        this.button0.x - widthDiff,
+        this.button7.y + heightDiff,
+        "numbers",
+        9
+      )
+      .setInteractive()
+      .setData("value", "del");
+    this.button3 = this.add
+      .image(this.button2.x + widthDiff, startPosY, "numbers", 2)
+      .setInteractive()
+      .setData("value", 3);
+    this.button6 = this.add
+      .image(
+        this.button5.x + widthDiff,
+        this.button3.y + heightDiff,
+        "numbers",
+        5
+      )
+      .setInteractive()
+      .setData("value", 6);
+    this.button9 = this.add
+      .image(
+        this.button8.x + widthDiff,
+        this.button6.y + heightDiff,
+        "numbers",
+        8
+      )
+      .setInteractive()
+      .setData("value", 9);
+    this.buttonOk = this.add
+      .image(
+        this.button0.x + widthDiff,
+        this.button9.y + heightDiff,
+        "numbers",
+        11
+      )
+      .setInteractive()
+      .setData("value", "ok");
   }
 }
